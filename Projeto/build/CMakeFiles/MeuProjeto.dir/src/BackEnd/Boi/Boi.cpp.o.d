6d0ae36aa5017c753ac392978b3080e15b7fbd6e
Quebec-Eric <quebec.ericjs@gmail.com>
2023-07-26 18:07:31 -0300

    ErroVacina

--- Projeto/build/CMakeFiles/MeuProjeto.dir/src/BackEnd/Boi/Boi.cpp.o.d
+++ Projeto/build/CMakeFiles/MeuProjeto.dir/src/BackEnd/Boi/Boi.cpp.o.d
@@ -1,3 +1,4 @@
 CMakeFiles/MeuProjeto.dir/src/BackEnd/Boi/Boi.cpp.o: \
  /home/quebec/Documents/FaTrabalho/Fazenda/Projeto/src/BackEnd/Boi/Boi.cpp \
- /usr/include/stdc-predef.h
+ /usr/include/stdc-predef.h \
+ /home/quebec/Documents/FaTrabalho/Fazenda/Projeto/src/BackEnd/Boi/Boi.h
